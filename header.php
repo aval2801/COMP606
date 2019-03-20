078d448542318cd4456a90c9dbacece9033f6705
AVAL <gohil.aval@gmail.com>
2019-03-20 20:55:42 +1300

    first commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+include_once('link.php');
+
+?>
+
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<ul class="nav navbar-nav">
+			<li><a href="registration.php">Registration</a></li>
+			<li><a href="login.php">Login</a></li>
+		</ul>
+	</div>
+</nav>
